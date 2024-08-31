d7f942d66d8dc0e3ad51b328b1cda3b0e22d3f88
Grey <greywhat2002@gmail.com>
2024-08-31 10:01:14 +0200

    password validation function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hash password"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if password is valid"""
+    if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+        return True
+    return False
