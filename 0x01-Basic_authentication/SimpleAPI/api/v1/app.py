9f8629e6476ebdd7743a5603ec7d225dd5979cfe
Grey-What <greywaht2002@gmail.com>
2024-09-02 14:23:12 +0000

    added reqest validation

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -14,6 +14,34 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """
+    run before each request
+    """
+    if auth is None:
+        pass
+    else:
+        excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/']
+
+        excluded = auth.require_auth(request.path, excluded_list)
+
+        if excluded:
+            if auth.authorization_header(request) is None:
+                abort(401, description='unauthorized')
+            if auth.current_user(request) is None:
+                abort(403, description='forbidden')
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -34,8 +34,14 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """
         sets authorization header for basic authentication
+
+        Args:
+        request: request object
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
