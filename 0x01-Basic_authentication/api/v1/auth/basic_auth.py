cf9e9aa596b3e295548b6c00a69e257f1265bab6
Grey-What <greywaht2002@gmail.com>
2024-09-03 13:52:30 +0000

    create class to handle basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+contains class BasicAuth and all it's methods
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class for Basic Authentication method
+    """
