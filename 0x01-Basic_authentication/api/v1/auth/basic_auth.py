a6512119bb2294cf340861d9ce4f41202eed9f9b
Grey-What <greywaht2002@gmail.com>
2024-09-03 14:18:59 +0000

    added method to basic authentication to extract base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,9 +4,23 @@ contains class BasicAuth and all it's methods
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     Class for Basic Authentication method
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        return BASE64 of Authorization Header for Basic Authentication
+
+        authorization_header (str): header of request
+        """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+
+        if authorization_header.startswith("Basic "):
+            return authorization_header.split(' ')[1]
+        return None
