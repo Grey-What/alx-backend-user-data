b22e663c1c4cd9c7934dd7b52fa9a8cac5675248
GreyWhat <greywhat2002@gmail.com>
2024-09-07 02:13:52 -0700

    creating in-memory Session ID storing to retrieve user id based on session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,21 @@ Module contains class for session authentication mechanism
 """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     session authenitcation mechanism class and it's methods
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session ID for a user"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
