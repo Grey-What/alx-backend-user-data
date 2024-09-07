0a9c538718bb949836250066952f38e7d5213add
GreyWhat <greywhat2002@gmail.com>
2024-09-07 01:36:54 -0700

    implmented class for session authentication mechanism

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module contains class for session authentication mechanism
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    session authenitcation mechanism class and it's methods
+    """
