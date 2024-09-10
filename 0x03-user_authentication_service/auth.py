992ba0c576efc57af25c3d774547e11d4ceec4ce
GreyWhat <greywhat2002@gmail.com>
2024-09-10 01:17:58 -0700

    implmented method to add new user to database

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,9 @@ This module will contain the Auth class and it's methods
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -18,3 +21,24 @@ def _hash_password(password: str) -> bytes:
 
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register a new user nad saving to database
+        """
+        try:
+            user_found = self._db.find_user_by(email=email)
+            if user_found:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed_pw = _hash_password(password)
+            user = self._db.add_user(email, hashed_pw)
+            return user
