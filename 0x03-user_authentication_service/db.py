a030d5eec7f4d09cd08dd4b1fdaee6f6b807a9fe
GreyWhat <greywhat2002@gmail.com>
2024-09-10 00:02:30 -0700

    intialization of the user class module

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,69 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Save a user to the databse and return the instance
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        self._session.add(new_user)
+        self._session.commit()
+
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        queries database for user filtered by keyword arguments provided
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update User attribute and commit changes to database
+        """
+        user = self.find_user_by(id=user_id)
+
+        for k, v in kwargs.items():
+            if not hasattr(user, k):
+                raise ValueError
+            setattr(user, k, v)
+
+        self._session.commit()
+        return None
