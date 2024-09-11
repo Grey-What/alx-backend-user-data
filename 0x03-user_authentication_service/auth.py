160cd3120cb83ab147b1cd551ed610e7514bb9e1
GreyWhat <greywhat2002@gmail.com>
2024-09-11 02:26:01 -0700

    fixed type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,7 +39,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> Union[None, User]:
         """
         register a new user nad saving to database
         """
